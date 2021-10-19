3baf016c307d6e0c2f3a5f511b33197e2a2c830f
dilexet <63055058+dilexet@users.noreply.github.com>
2021-10-19 21:07:41 +0300

    херня какая-то 3

--- courseproject.ui/src/Authorize/Login/container/LoginContainer.tsx
+++ courseproject.ui/src/Authorize/Login/container/LoginContainer.tsx
@@ -2,52 +2,57 @@
 import React, {useState} from 'react';
 import Login from "../component/Login";
 import {LoginContainerProps} from "../types/LoginContainerProps";
-import {useTypeSelector} from "../../../Shared/hooks/useTypeSelector";
+import * as yup from 'yup';
+import {SchemaOf} from "yup";
+import {LoginFormValues} from "../types/LoginFormValues";
 import {useActions} from "../hooks/useActions";
-import {ILoginModel} from "../store/types/ILoginModel";
+import * as actions from "../store/action-creator/loginActions";
+import {useTypeSelector} from "../../../Shared/hooks/useTypeSelector";
+import {RootState} from "../../../Shared/store/redusers";
+import {connect} from "react-redux";
 
-const initialFieldValues: ILoginModel = {
-    Login: '',
-    Password: ''
-}
-
-
-const LoginContainer: React.FC<LoginContainerProps> = ({t}) => {
+const LoginContainer: React.FC<LoginContainerProps> = ({t, signIn, loginState}) => {
     const [redirect, setRedirect] = useState<boolean>(false);
-    const [values, setValues] = useState<ILoginModel>(initialFieldValues);
-    const {sendLogin} = useActions();
 
-    const state = useTypeSelector(state => state.login);
+    // const {sendLogin} = useActions();
+    // const loginState = useTypeSelector(x => x.login);
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log(values)
-        sendLogin(values)
-        setRedirect(true)
-        resetForm()
+
+
+
+    // TODO: add regular expression from ./constants/RegularExpression
+    const validationSchema: SchemaOf<LoginFormValues> = yup.object({
+        Login: yup
+            .string()
+            .min(3, 'Login should be of minimum 5 characters length')
+            .max(40, 'Login is too long')
+            .required('Please enter your login'),
+        Password: yup
+            .string()
+            .min(3, 'Password should be of minimum 5 characters length')
+            .max(40, 'Password is too long')
+            .required('Please enter your password'),
+    });
+
+    function handleSubmit (values: LoginFormValues) {
+        signIn(values)
+        // setRedirect(true)
+        console.log("handleSubmit:")
+        console.log(loginState)
     }
-
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const {name, value} = event.target;
-        const fieldValue = {[name]: value}
-        setValues({
-            ...values,
-            ...fieldValue
-        })
-    }
-
-    const resetForm = () => {
-        setValues({
-            ...initialFieldValues
-        })
-    }
-
-    console.log(state);
 
     return (
-        <Login values={values} handleInputChange={handleInputChange} resetForm={resetForm} t={t} redirect={redirect} handleSubmit={handleSubmit}/>
+        <Login handleSubmit={handleSubmit} loginState={loginState} validation={validationSchema} t={t}
+               redirect={redirect}/>
     );
 }
 
+const mapStateToProps = (state: RootState) => ({
+    loginState: state.login,
+})
 
-export default LoginContainer;
+const mapActionToProps = {
+    signIn: actions.SignIn,
+}
+
+export default connect(mapStateToProps, mapActionToProps)(LoginContainer);
--- courseproject.ui/src/Authorize/Login/types/LoginContainerProps.ts
+++ courseproject.ui/src/Authorize/Login/types/LoginContainerProps.ts
@@ -1,5 +1,9 @@
 import {TFunction} from "react-i18next";
+import {LoginState} from "../store/types/LoginTypes";
+import {LoginFormValues} from "./LoginFormValues";
 
 export type LoginContainerProps = {
-    t: TFunction,
+    t: TFunction;
+    loginState: LoginState;
+    signIn: (data: LoginFormValues) => void;
 }
